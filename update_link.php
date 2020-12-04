dbf7597c3533a0f5081cd05c9542a5de51c7e764
Luis Cabrera Benito <parzibyte@gmail.com>
2020-12-04 00:44:52 -0600

    Al actualizar también se usa el entero

--- update_link.php
+++ update_link.php
@@ -8,6 +8,6 @@ if (!isset($_POST["id"]) || !isset($_POST["title"]) || !isset($_POST["real_link"
 }
 
 include_once "vendor/autoload.php";
-$instantRedirect = isset($_POST["instant_redirect"]);
+$instantRedirect = intval(isset($_POST["instant_redirect"]));
 LinkController::update($_POST["id"], $_POST["title"], $_POST["real_link"], $instantRedirect);
 header("Location: link_management.php");
